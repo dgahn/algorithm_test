37a14e34d3c70514f735f12c7ad75f347b2476a1
dgahn <dgahn@icloud.com>
2025-08-09 11:06:50 +0900

    add CreateArray2

--- src/main/kotlin/me/dgahn/programmers/basic/day07/CreateArray2.kt
+++ src/main/kotlin/me/dgahn/programmers/basic/day07/CreateArray2.kt
@@ -1,4 +1,27 @@
 package me.dgahn.programmers.basic.day07
 
 class CreateArray2 {
+    fun solution(arr: IntArray): IntArray {
+        var stk = mutableListOf<Int>()
+        // i를 0으로 초기화
+        var i = 0
+
+        // i가 arr의 길이보다 작으면
+        while (true) {
+            if (i < arr.size) {
+                if (stk.isEmpty()) {
+                    stk.add(arr[i])
+                    i += 1
+                } else if (stk.last() < arr[i]) {
+                    stk.add(arr[i])
+                    i += 1
+                } else if (stk.last() >= arr[i]) {
+                    stk.removeLast()
+                }
+            } else {
+                break
+            }
+        }
+        return stk.toIntArray()
+    }
 }
